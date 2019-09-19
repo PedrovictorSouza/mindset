49ca04da300b7cb474be13cf6139feeaa77fd296
Pedro Victor Souza Da Cunha <pedrovictor@MacBook-Pro-de-Pedro.local>
2019-09-19 16:39:48 -0300

    atualizacoes

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ var x = 2
 const nav = document.getElementById("nav")
 
 function carrossel() {
-  var frame = document.getElementById('frame')
-  frame.src = `imgs/art0${x}.png`
-  x++
+    var frame = document.getElementById('frame')
+    frame.src = `imgs/art0${x}.png`
+    x++
 }
 
 
@@ -12,57 +12,57 @@ function carrossel() {
 
 function teste(offset) {
 
-  var posters = [
-    document.getElementById("poster-a"),
-    document.getElementById("poster-b"),
-    document.getElementById("poster-c")
-  ];
+    var posters = [
+        document.getElementById("poster-a"),
+        document.getElementById("poster-b"),
+        document.getElementById("poster-c")
+    ];
 
 
 
-  if(offset === 0) {
-     
+    if (offset === 0) {
 
-    posters[1].style.top = -18 + "vh"
-    posters[0].style.top = -28 + "vh"
-    posters[2].style.top =  -5 + "vh"
-    posters[0].style.zIndex = "1003"
-    posters[2].style.zIndex = "auto"
-    
-    $('#essencia_btn_a').attr("src","imgs/jukebox-04.png")
 
-  }
+        posters[1].style.top = -18 + "vh"
+        posters[0].style.top = -28 + "vh"
+        posters[2].style.top = -5 + "vh"
+        posters[0].style.zIndex = "1003"
+        posters[2].style.zIndex = "auto"
 
-  if(offset === 1) {
-    
-    posters[0].style.top = -18 + "vh"
-    posters[1].style.top = -28 + "vh"
-    posters[2].style.top =  -5 + "vh"
-    posters[1].style.zIndex = "1003"
-    posters[0].style.zIndex = "auto"
-    posters[2].style.zIndex = "auto"
+        $('#essencia_btn_a').attr("src", "imgs/jukebox-04.png")
 
-    $('#essencia_btn_b').attr("src","imgs/jukebox-05.png")
-  }
+    }
 
-  if(offset === 2) {
-    
-    posters[2].style.top = -28 + "vh"
-    posters[1].style.top = -18 + "vh"
-    posters[0].style.top = -5 + "vh"
-    posters[2].style.zIndex = "1003"
-    posters[1].style.zIndex = "1"
-    posters[0].style.zIndex = "auto"
+    if (offset === 1) {
 
-    $('#essencia_btn_c').attr("src","imgs/jukebox-06.png")
-  }
+        posters[0].style.top = -18 + "vh"
+        posters[1].style.top = -28 + "vh"
+        posters[2].style.top = -5 + "vh"
+        posters[1].style.zIndex = "1003"
+        posters[0].style.zIndex = "auto"
+        posters[2].style.zIndex = "auto"
+
+        $('#essencia_btn_b').attr("src", "imgs/jukebox-05.png")
+    }
+
+    if (offset === 2) {
+
+        posters[2].style.top = -28 + "vh"
+        posters[1].style.top = -18 + "vh"
+        posters[0].style.top = -5 + "vh"
+        posters[2].style.zIndex = "1003"
+        posters[1].style.zIndex = "1"
+        posters[0].style.zIndex = "auto"
+
+        $('#essencia_btn_c').attr("src", "imgs/jukebox-06.png")
+    }
 }
 
 
 function testeB(offset) {
-  $('#essencia_btn_a').attr("src","imgs/jukebox-01.png")
-  $('#essencia_btn_b').attr("src","imgs/jukebox-02.png")
-  $('#essencia_btn_c').attr("src","imgs/jukebox-03.png")
+    $('#essencia_btn_a').attr("src", "imgs/jukebox-01.png")
+    $('#essencia_btn_b').attr("src", "imgs/jukebox-02.png")
+    $('#essencia_btn_c').attr("src", "imgs/jukebox-03.png")
 }
 
 
@@ -99,99 +99,116 @@ var imageChange = document.getElementById('home-img');
 })()*/
 
 
-$(".nav-a").click(function(){
-  $(".nav-a").removeClass('nav-active')
-  $(this).addClass('nav-active')
+$(".nav-a").click(function() {
+    $(".nav-a").removeClass('nav-active')
+    $(this).addClass('nav-active')
 })
 
 $(document).ready(function() {
-  $('#lettering').show('slow')
-  var phrases = new Array("[A ARTE DE JUNTAR AS METADES E FAZER UM INTEIRO]","[A ARTE DE PENSAR EM DOBRO PARA FAZER DIREITO]", "[A ARTE DE FAZER UMA METADE DO TEMPO TRABALHAR PARA A OUTRA METADE DO TEMPO]", "[A ARTE DE NAO DESPERDICAR TEMPO COM ATALHOS]", "[A ARTE DE NAO ACHAR QUE TUDO PRECISA SER UMA ARTE]", "[A ARTE DE DESISTIR NA METADE QUANDO A IDEIA JA NAO FAZ SENTIDO]", "[A ARTE DE PERGUNTAR EM DOBRO QUANDO AS RESPOSTAS ESTAO PELA METADE]")
-
-  setInterval(function(){
     $('#lettering').show('slow')
-    var numB = Math.floor(Math.random() * phrases.length);
-    $('#lettering').html(phrases[numB])
-  }, 3000)
+    var phrases = new Array("[A ARTE DE JUNTAR AS METADES E FAZER UM INTEIRO]", "[A ARTE DE PENSAR EM DOBRO PARA FAZER DIREITO]", "[A ARTE DE FAZER UMA METADE DO TEMPO TRABALHAR PARA A OUTRA METADE DO TEMPO]", "[A ARTE DE NAO DESPERDICAR TEMPO COM ATALHOS]", "[A ARTE DE NAO ACHAR QUE TUDO PRECISA SER UMA ARTE]", "[A ARTE DE DESISTIR NA METADE QUANDO A IDEIA JA NAO FAZ SENTIDO]", "[A ARTE DE PERGUNTAR EM DOBRO QUANDO AS RESPOSTAS ESTAO PELA METADE]")
 
-  /*
-  $(".balao").on("mouseover", function(){
-    var balloonText = ["Imprevisibilidade não é a norma."]
-    $(".balao").html("OUVIR")
-    $(this).html(balloonText[0])
-  });
-  */
+    setInterval(function() {
+        $('#lettering').show('slow')
+        var numB = Math.floor(Math.random() * phrases.length);
+        $('#lettering').html(phrases[numB])
+    }, 3000)
+
+    /*
+    $(".balao").on("mouseover", function(){
+      var balloonText = ["Imprevisibilidade não é a norma."]
+      $(".balao").html("OUVIR")
+      $(this).html(balloonText[0])
+    });
+    */
 });
 
-$(".mouth").hover(function(){
-  $(".mouth").css("background-image", "url('imgs/orelha.png')")
-  $(".mouth").css("background-color", "red")
-  $(".mouth").css("background-size", "25vw")
-  $(".o_que_e_p").css("display", "none")
-  $(this).css({"background-image": "url('https://media.giphy.com/media/l378iZdJpAxwwdY52/giphy.gif')","background-color":"blue","background-size":"cover"})
+$(".mouth").hover(function() {
+    $(".mouth").css("background-image", "url('imgs/orelha.png')")
+    $(".mouth").css("background-color", "red")
+    $(".mouth").css("background-size", "25vw")
+    $(".o_que_e_p").css("display", "none")
+    $(this).css({ "background-image": "url('https://media.giphy.com/media/l378iZdJpAxwwdY52/giphy.gif')", "background-color": "blue", "background-size": "cover" })
 });
 
 function menuEssencia() {
- document.getElementById("contexto").style.display = "block"
+    document.getElementById("contexto").style.display = "block"
 }
 
 var video = document.getElementById("myVideo");
 var btn = document.getElementById("myBtn");
 
 function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
+    }
 }
 
 
- function changeBalloon(e, x){
+function changeBalloon(e, x) {
     var balloonText = [
         "Ágil é diferenciar causa de consequência.",
-			  "Imprevisibilidade não é a norma.",
-			  "Coisas mudam.",
-			  "Capacidade é finita.",
-			  "P.O. sem atitude não atravessa a rua.",
-			  "O Scrum não é Master à toa. Aja como.",
-			  "Backlog é mais que uma lista de atividades priorizadas.",
-			  "O cliente nem sempre tem razão. Debata.",
-			  "Comunicação é a alma.",
-			  "Quem procrastina, levanta a mão. Ué, não procrastinou agora.",
-			  "A duração da Sprint é uma arte.",
-			  "Não, não é sobre ser mais veloz.",
-			  "Kanban estático é exposição de arte. Faça gestão visual.",
-			  "Pensando que era impossível, esqueceu de colocar no backlog.",
-			  "Confiança vem do hábito.",
-			  "O erro é normal. Anormal é não fazer retrospectiva.",
-			  "Coloca pra funcionar.",
-			  "Fazer de qualquer jeito não é ser ágil. ",
-			  "Tente não usar a palavra ágil. Ela só atrapalha.",
-			  "Nos dias de entrega, faça a entrega.",
-    ]  
+        "Imprevisibilidade não é a norma.",
+        "Coisas mudam.",
+        "Capacidade é finita.",
+        "P.O. sem atitude não atravessa a rua.",
+        "O Scrum não é Master à toa. Aja como.",
+        "Backlog é mais que uma lista de atividades priorizadas.",
+        "O cliente nem sempre tem razão. Debata.",
+        "Comunicação é a alma.",
+        "Quem procrastina, levanta a mão. Ué, não procrastinou agora.",
+        "A duração da Sprint é uma arte.",
+        "Não, não é sobre ser mais veloz.",
+        "Kanban estático é exposição de arte. Faça gestão visual.",
+        "Pensando que era impossível, esqueceu de colocar no backlog.",
+        "Confiança vem do hábito.",
+        "O erro é normal. Anormal é não fazer retrospectiva.",
+        "Coloca pra funcionar.",
+        "Fazer de qualquer jeito não é ser ágil. ",
+        "Tente não usar a palavra ágil. Ela só atrapalha.",
+        "Nos dias de entrega, faça a entrega.",
+    ]
 
     $(".balao").html("")
-    $(".balao").css({"background":"url(imgs/balao-03.svg) no-repeat center","color":"red","transition":"1s"})
+    $(".balao").css({ "background": "url(imgs/balao-03.svg) no-repeat center", "color": "red", "transition": "1s" })
     $(e.toElement).html(balloonText[x])
-    $(e.toElement).css({"background":"url(imgs/balao-02.svg) no-repeat center", "color":"red",})
-    
+    $(e.toElement).css({ "background": "url(imgs/balao-02.svg) no-repeat center", "color": "red", })
+
 }
 
 function changeToNormal() {
-  $(".balao").html("")
-  $(".balao").css({"background":"url(imgs/balao-01.svg) no-repeat center", "color":"black", "transition":"1s"})
+    $(".balao").html("")
+    $(".balao").css({ "background": "url(imgs/balao-01.svg) no-repeat center", "color": "black", "transition": "1s" })
 }
 
 function openMenu() {
-  nav.style.display = "block"
+    nav.style.display = "block"
 }
 
 function closeMenu() {
-  nav.style.display = "none"
+    nav.style.display = "none"
 }
 
 
+var contextoImg = document.getElementById("contextoImg")
+var img_id = "c"
 
+function flipImg() {
+    if (img_id == 'c') {
+        contextoImg.src = 'imgs/art02.png'
+        img_id = 'm'
+    } else if (img_id == 'm') {
+        contextoImg.src = 'imgs/art03.png'
+        img_id = 'd'
+    } else if (img_id == 'd') {
+        contextoImg.src = 'imgs/art01.png'
+        img_id = 'c'
+    }
+    setTimeout(flipImg, 500);
+}
+
+flipImg()
